15e07dc8b1bb7974fc6189100076ad0b12c89866
t0malbrecht <tom.albrecht@ewetel.net>
2020-12-17 11:53:15 +0100

    fixed combinations methode

--- src/services/Combinations.java
+++ src/services/Combinations.java
@@ -17,28 +17,18 @@ public class Combinations {
         int fourthDigit;
 
         for(int i=0;i<range.length;i++){
-            firstDigit = range[i];
-            secondDigit = range[0];
-            thirdDigit = range[0];
-            fourthDigit = range[0];
-            combinations.add(new int[] {firstDigit,secondDigit, thirdDigit, fourthDigit});
-            for(int j=1;j<range.length;j++){
-                secondDigit = range[j];
-                thirdDigit = range[0];
-                fourthDigit = range[0];
-                combinations.add(new int[] {firstDigit,secondDigit, thirdDigit, fourthDigit});
-                for(int k=0;k<range.length;k++){
-                    thirdDigit = range[k];
-                    fourthDigit = range[0];
-                    combinations.add(new int[] {firstDigit,secondDigit, thirdDigit, fourthDigit});
-                    for(int l=0;l<range.length;l++) {
-                        fourthDigit = range[l];
-                        combinations.add(new int[]{firstDigit, secondDigit, thirdDigit, fourthDigit});
+            for(int j=0;j<range.length;j++){
+                for(int k=0;k<range.length;k++) {
+                        for (int l = 0; l < range.length; l++) {
+                                firstDigit = range[i];
+                                secondDigit = range[j];
+                                thirdDigit = range[k];
+                                fourthDigit = range[l];
+                                combinations.add(new int[]{firstDigit, secondDigit, thirdDigit, fourthDigit});
+                            }
+                        }
                     }
                 }
-            }
-
-        }
         savedCombinations = combinations;
         return combinations;
     }
